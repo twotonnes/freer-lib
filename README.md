bc84618b829557b2cf9e85fd75e5136f8db02ffd
Niels Nijholt <niels@nielsnijholt.nl>
2026-02-04 13:54:07 +0100

    Changed name to freer-monad

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# effect-lib
-A Racket package that enables the usage of a sort of algebraic effects using a Freer monad.
+# freer-lib
+A Racket package that enables the usage of algebraic effects using the Freer monad.
 
 ## License
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 
-(define collection "effect-lib")
+(define collection "freer-lib")
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc"))
+(define pkg-desc "A collection of libraries for writing freer monads in Racket.")
+(define pkg-authors '("Niels Nijholt"))
 (define license "MIT")
