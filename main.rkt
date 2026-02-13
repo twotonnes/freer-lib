ead2fcbdf96ff05f0a88e10075a8e8532c8d965b
Niels Nijholt <niels@nielsnijholt.nl>
2026-02-13 15:21:52 +0100

    Correction: added try-catch as output to main.rkt

--- main.rkt
+++ main.rkt
@@ -2,8 +2,10 @@
 
 (provide
   (all-from-out "freer-monad.rkt")
-  (all-from-out "effects/main.rkt"))
+  (all-from-out "effects/main.rkt")
+  (all-from-out "try-catch.rkt"))
 
 (require
   "freer-monad.rkt"
-  "effects/main.rkt")
+  "effects/main.rkt"
+  "try-catch.rkt")
